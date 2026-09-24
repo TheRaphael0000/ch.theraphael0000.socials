2bd82f8978cb740fdbb33a2acb35f3cf3dc33c6e
Raphael Margueron <raphael.margueron@gmail.com>
2026-09-24 16:00:04 +0200

    fix config for nginx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ if [ -n "$TZ" ] ; then
     ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
 fi
 if [ ! -e /data/snac2/server.json ]; then
-    echo -ne "0.0.0.0\r\n8001\r\nlocalhost\r\n\r\n\r\n" | snac init /data/snac2
+    echo -ne "0.0.0.0\r\n8001\r\nsocials.theraphael0000.ch\r\n\r\n\r\n" | snac init /data/snac2
     snac adduser /data/snac2 raphael
 fi
 
