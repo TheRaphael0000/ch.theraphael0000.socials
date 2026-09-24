d937d7db37dace84b379cf16be87d33b35536c9c
Raphael Margueron <raphael.margueron@gmail.com>
2026-09-24 15:22:09 +0200

    basic config for snac with docker

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+if [ -n "$TZ" ] ; then
+    ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime && echo "$TZ" > /etc/timezone
+fi
+if [ ! -e /data/snac2/server.json ]; then
+    echo -ne "0.0.0.0\r\n8001\r\nlocalhost\r\n\r\n\r\n" | snac init /data/snac2
+    snac adduser /data/snac2 raphael
+fi
+
+snac httpd /data/snac2
